dcd3e186f63f2a9bcf1c23a31b8a76bab25760c7
ccvacad <ccvacad@unal.edu.co>
2017-04-17 23:48:40 -0500

    IronHacks v4

--- README.md
+++ README.md
@@ -1,46 +1,37 @@
-Readme Introduction
+Find Home In Chicago
 
 Site: https://ccvacad.github.io/IronHacks/
 
-This is a guide for how to set up your README.md file for your IronHack Application. The file should have following fields:
+Author:
+Christian Camilo Vaca Daza
+ccvacad@unal.edu.co
+Github: https://github.com/ccvacad
 
-    Name of your Application: Find Home In Chicago
 
-    Keywords You should include at least 3 keywords for your project to identify the features of your project, such as: 
+Introduction
 
-    Description of the datasets and function design
+Find Home In Chicago solves the problem posed by IronHacks: “Find me a safe and affordable place to rent near Department of Computer Science – University of Illinois, Chicago”
 
-    [ Affordable Rental Housing Developments] [https://data.cityofchicago.org/api/views/s6ha-ppgi/rows.json] [json] [data columns used] [data amount] Please provide a name+link+basicInfo to each dataset you have used.
-    [N] Do you use the primary dataset ”online climate data” from data.gov?
-    [Y] [List] Are all these datasets from data.gov or data.indy.gov? If not, where are they coming from (links)?
+Keywords
+Chicago, Rent house, Price, Distance, Crimes, Security, University of Illinois at Chicago
 
-    Brief Description
 
-    Use a paragraph to introduce your project.
+Datasets
 
-Fill in the structued description:
+Crimes: https://data.cityofchicago.org/resource/6zsd-86xi.json
+Rental: https://data.cityofchicago.org/api/views/s6ha-ppgi/rows.json
 
-    Map View:
-        [Y] Basic Map with specific location (your map is located in a meaningful place, city of west lafayette for example)
-        [Y] Markers for location of markets
-        [N] Labels for markets' names
-        [N] InfoWindow to show detail information of a market
-        [N] [describe] Any other cover on the map (for example, cloud cover to show the weather effect)
 
-    Data Visualization:
-        [N] [describe] Use Graph? What is the type? (bar chart, pie chart, radar chart ...)
-        [N] [List] Any interaction available on the graph? List them (enable click on numbers, drag on lines, change time/variables ...)
+Description:
 
-    Interaction Form:
-        [Y/N] [List] Any information output? list them. (text field, text area, label, plain HTML ...)
-        [Y/N] [List] Any operation option (filters)? List them. (search markets, search vegetables, filter based on price, sort based on convenience ...)
-        [Y/N] [List] Any information input? List them. (comments, markers, user preference ...)
-        [Y/N] [List] Interaction with Map? List them. (filter on price will affect map markers, sort on price will affect map markers, ...)
-        [Y/N] [List] Interaction with data visualization? List them. (filter, sort, set variables ...)
+Find Home In Chicago provide information of the places available for rent taking into account the rental cost, security of the nearby areas and distance to the university.
 
-    Build Case How can we build and access your project on a Linux/Unix machine if you use external dependencies besides HTML/CSS/Javascript? List the dependencies you used, such as python, node.js, etc. List the steps we should follow to build the project.
+Each marker of the places available for rent provide you information of contact, address, phone, distance in modal box.
 
-    Test Case Which browsers did you test your project on? Chrome, IE, Edge, Safari, or Firefox?
+The markers of the crimes provide you information of the type of crime committed on the area. 
 
-    Additional information You Want to Share with Us E.g. any problems you faced/fixed, where you reached out to for help, etc.
+Also,  It gives us a Heat Map about the crimes in Chicago.
 
+
+Past submission:
+https://github.com/ccvacad/IronHacks
--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,81 @@
+var map;
+var markerUniversity;
+
 function initMap(){
-	var lt = 41.8708;
-	var lg = -87.6505;
-	var title = 'Department of Computer Science - University of Illinois';
-	var map = new google.maps.Map(d3.select("#map").node(), {
-  		zoom: 15,
-	  	center: new google.maps.LatLng(lt, lg),
-	  	mapTypeId: google.maps.MapTypeId.TERRAIN,
-	  	mapTypeControlOptions: {
-        	style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-       		position: google.maps.ControlPosition.LEFT_BOTTOM
-    	}
+	var data = {
+		name: 'Department of Computer Science - University of Illinois',
+		lat: 41.8708, 
+		lon: -87.6505
+	}
+	map = new google.maps.Map(d3.select("#map").node(), {
+  		zoom: 12,
+	  	center: new google.maps.LatLng(data.lat, data.lon),
 	});
 
-	loadPoints(map);
-	makePoint(map, lt, lg, title, "img/university.png");
-}
+	var icon = {
+		url: "img/university.png",
+	    scaledSize: new google.maps.Size(42, 42), // scaled size
+	    origin: new google.maps.Point(0,0), // origin
+	    anchor: new google.maps.Point(0, 0) // anchor	
 
-function loadPoints(map){
+	};
+
+	markerUniversity = new google.maps.Marker({ //Line 1
+		position: {lat: data.lat, lng: data.lon}, //Line2: Location to be highlighted
+		map: map,//Line 3: Reference to map object
+		title: data.name, //Line 4: Title to be given
+		icon: icon
+	});
+
+	loadPoints();
+
+};
+
+function loadPoints(){
 	$.getJSON("https://data.cityofchicago.org/api/views/s6ha-ppgi/rows.json ", function (data) {
 		for(i in data["data"]){
-			makePoint(map, parseFloat(data["data"][i][19]), parseFloat(data["data"][i][20]), "", "img/home-pointer-icon.png");
+			var mapData = {
+				name : data["data"][i][11],
+				addres: data["data"][i][12],
+				phone: data["data"][i][14],
+				type: data["data"][i][10],
+				area: data["data"][i][8],
+				number: data["data"][i][16],
+				company: data["data"][i][15],
+				lat: parseFloat(data["data"][i][19]),
+				lon: parseFloat(data["data"][i][20]),
+				img: 'https://maps.googleapis.com/maps/api/streetview?' + 'location=' + data["data"][i][12] + '&size=600x300' + '&key=AIzaSyDKNgdNc2Fw6SMLBpcYYIPZr4mbx03w2CQ',
+				price: 0
+			};
+			makePoint(mapData, "img/home-pointer-icon.png");
 		}
 	});
-}
+};
 
-function makePoint(map, lt, lg, title, ico){
+var infWindow;    
+var  placesPoints = [];
+function makePoint(data, ico){
+	var contentString = 
+		'<div id="content">'+
+      		'<div id="siteNotice">'+
+      		'</div>'+
+      		'<h5 id="firstHeading" class="firstHeading">' + data.name + '</h5>'+
+	      	'<div id="bodyContent">'+
+	      		'<img src = "' + data.img + '" width = "200">' + 
+	      		'<br><b>Addres: </b>' + data.addres +
+	      		'<br><b>Phone: </b>' + data.phone +
+	      		'<!-- Trigger the modal with a link -->' +
+				'<br><a data-toggle="modal" href="#myModal">More info...</a>' +
+	      	'</div>'+
+      	'</div>';
+
+
+    var infowindow = new google.maps.InfoWindow({
+    	content: contentString,
+    	maxWidth: 250
+  	});
+
+
 	var icon = {
 		url: ico,
 	    scaledSize: new google.maps.Size(42, 42), // scaled size
@@ -33,79 +84,176 @@ function makePoint(map, lt, lg, title, ico){
 
 	};
 	var marker = new google.maps.Marker({ //Line 1
-		position: {lat: lt, lng: lg}, //Line2: Location to be highlighted
+		position: {lat: data.lat, lng: data.lon}, //Line2: Location to be highlighted
 		map: map,//Line 3: Reference to map object
-		title: title, //Line 4: Title to be given
+		title: data.name, //Line 4: Title to be given
 		icon: icon
 	});
-}
 
-/*function displayMenu(){
-	var button = document.getElementById('menuButton');
-	var menu = document.getElementById('menu');
 
-	if(button.value == 1){
-		button.firstChild.data = "X";
-		menu.style.visibility = "visible";
-		button.style.marginLeft = "270px";
-		button.value = 0;
+	marker.addListener('click', function() {
+	   	if (infWindow) { infWindow.close();}
+        infowindow.open(map, marker);
+        infWindow = infowindow;
+        document.getElementById("modalName").innerHTML = data.name;
+        document.getElementById('modalImg').src = data.img;
+        document.getElementById('modalAddres').innerHTML = data.addres;
+        document.getElementById('modalPhone').innerHTML = data.phone;
+        document.getElementById('modalCompany').innerHTML = data.company;
+        document.getElementById('modalType').innerHTML = data.type;
+        document.getElementById('modalArea').innerHTML = data.area;
+        distance(data.lat, data.lon);
+  	});
+
+  	placesPoints.push(marker);
+};
+
+function distance(lt, lg){
+	var source = {lat: 41.8708, lng: -87.6505};
+	var destination = {lat: lt, lng: lg}
+	var service = new google.maps.DistanceMatrixService();
+	var res;
+    service.getDistanceMatrix({
+        origins: [source],
+        destinations: [destination],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false
+    }, function (response, status) {
+        if (status == google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status != "ZERO_RESULTS") {
+            var distance = response.rows[0].elements[0].distance.text;
+            var duration = response.rows[0].elements[0].duration.text;
+            res = distance;
+        } else {
+            res = "Unable to find the distance via road.";
+        }
+        document.getElementById('modalDistance').innerHTML = res;
+    });
+};
+
+function options(){
+	if(document.getElementById("crimesOption").checked == true)
+		crimes(true);
+	else
+		crimes(false);
+	if(document.getElementById("universityOption").checked == true)
+		markerUniversity.setMap(map);
+	else
+		markerUniversity.setMap(null);
+
+	if(document.getElementById("placesOption").checked == true)
+		for(i = 0; i < placesPoints.length; i++)
+			placesPoints[i].setMap(map);
+	else
+		for(i = 0; i < placesPoints.length; i++)
+			placesPoints[i].setMap(null);
+
+	if(document.getElementById("priceOption").checked == true);
+};
+
+var crimePoints = [];
+var crimesHeatMap;
+
+function makePointsCrimes(data){
+	var icon = {
+		url: "img/iconCrimes.png",
+	    scaledSize: new google.maps.Size(42, 42), // scaled size
+	    origin: new google.maps.Point(0,0), // origin
+	    anchor: new google.maps.Point(0, 0) // anchor	
+	};
+
+	var crimeMarker = new google.maps.Marker({
+        position: data.location,
+        map: map,
+        icon: icon,
+        title: data.type
+    });
+
+  	crimeMarker.addListener('click', function() {
+		new google.maps.InfoWindow({
+				content: data.type
+		}).open(map, crimeMarker);
+	});
+
+	crimePoints.push(crimeMarker);
+};
+
+function visibleMarkerPointsCrimes(){
+	var state;
+	if(document.getElementById("btnCrimesMarker").innerHTML == 'Hide Marker'){
+		document.getElementById("btnCrimesMarker").innerHTML = 'Show Marker';
+		state = null;
 	}
 	else{
-		button.innerHTML = "&#9776;";
-		menu.style.visibility = "hidden";
-		button.style.marginLeft = "0px";
-		button.value = 1;
+		document.getElementById("btnCrimesMarker").innerHTML = 'Hide Marker';
+		state = map;
+	}
+
+	for(i = 0; i < crimePoints.length; i++){
+		crimePoints[i].setMap(state);
 	}
 }
 
-/*function loadApp(map){
-	// Load the station data. When the data comes back, create an overlay.
-	d3.json("https://data.cityofchicago.org/api/views/s6ha-ppgi/rows.json", function(error, data) {
-	  	if (error) throw error;
+function heatmap(){
+	if(document.getElementById("btnHeatMap").innerHTML == 'Hide Heat Map'){
+		document.getElementById("btnHeatMap").innerHTML = 'Show Heat Map';
+	}
+	else
+		document.getElementById("btnHeatMap").innerHTML = 'Hide Heat Map';
+		toggleHeatmap();
+};
 
-	  	var overlay = new google.maps.OverlayView();
+function toggleHeatmap() {	
+	if(crimesHeatMap != undefined)
+		crimesHeatMap.setMap(crimesHeatMap.getMap() ? null : map);
+};
 
-	  	// Add the container when the overlay is added to the map.
-	  	overlay.onAdd = function() {
-	    	var layer = d3.select(this.getPanes().overlayLayer).append("div")
-	        	.attr("class", "points");
-	    	// Draw each marker as a separate SVG element.
-	    	// We could use a single SVG, but what size would it have?
-	    	overlay.draw = function() {
-	     		var projection = this.getProjection(),
-	          	padding = 10;
 
-	      		var marker = layer.selectAll("svg")
-		          	.data(d3.entries(data["data"]))
-		          	.each(transform) // update existing markers
-		        	.enter().append("svg")
-		          	.each(transform)
-		          	.attr("class", "marker");
+function crimes(op){
+	if(op){	
+		document.getElementById("btnHeatMap").style.visibility = 'visible';
+		document.getElementById("btnHeatMap").innerHTML = 'Hide Heat Map'
+		document.getElementById("btnCrimesMarker").style.visibility = 'visible';
+		document.getElementById("btnCrimesMarker").innerHTML = 'Hide Marker'
+		//request places data
+		var xmlhttp = new XMLHttpRequest();
+		var url = "https://data.cityofchicago.org/resource/6zsd-86xi.json"
+		xmlhttp.open("GET", url, true);
+		xmlhttp.send();
+		var locations = [];
+		xmlhttp.onreadystatechange = function() {
+		    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		      	var myArr = xmlhttp.responseText;
+		      	var text = myArr;
+		      	var json = JSON.parse(text);
+		      	for (var i = 0; i<json.length; i++) {
+		          	var data = {
+		            	type: json[i].primary_type,
+		            	location: new google.maps.LatLng(parseFloat(json[i].latitude), parseFloat(json[i].longitude))
+		          	};
+		          	
+		          	makePointsCrimes(data);
 
-			    // Add a circle.
-			    marker.append("circle")
-		          	.attr("r", 4.5)
-		          	.attr("cx", padding)
-		          	.attr("cy", padding);
-
-			    // Add a label.
-			    marker.append("text")
-			        .attr("x", padding + 7)
-		          	.attr("y", padding)
-		          	.attr("dy", ".31em")
-		          	.text(function(d) {return d.key; });
-
-			    function transform(d) {
-			        d = new google.maps.LatLng(d.value[19], d.value[20]);
-			        d = projection.fromLatLngToDivPixel(d);
-			        return d3.select(this)
-			        	.style("left", (d.x - padding) + "px")
-			            .style("top", (d.y - padding) + "px");
-			    }
-	    	};
-	  	};
-
-	  	// Bind our overlay to the map…
-	  	overlay.setMap(map);
-	});
-}*/
+		      		if (json[i].latitude != undefined) {
+		        		locations.push(data.location);
+		      		}
+		      	}
+		      	crimesHeatMap = new google.maps.visualization.HeatmapLayer({
+      				data : locations,
+      				map: map
+   				});
+		    }
+		}
+	}
+	else{
+		document.getElementById("btnHeatMap").style.visibility = 'hidden';
+		document.getElementById("btnCrimesMarker").style.visibility = 'hidden';
+		for(i = 0; i < crimePoints.length; i++){
+			crimePoints[i].setMap(null);
+		}
+		crimePoints = [];
+		if(crimesHeatMap != undefined)
+			crimesHeatMap.setMap(null);
+	}
+};
