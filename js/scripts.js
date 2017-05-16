237beddc97e207f5666278e4f83a52454c9746b5
Christian Camilo Vaca Daza <ccvacad@unal.edu.co>
2017-05-16 12:46:54 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -282,17 +282,24 @@ function crimes(op){
 };
 
 function weatherData(){
+	//create a new httprequest for this session
 	var xmlhttp = new XMLHttpRequest();
-	var url = "//api.openweathermap.org/data/2.5/weather?q=chicago&appid=6aa0bdb1f586c5630d60b6237dfce45c";
+	//json format data resource url 
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=chicago&appid=6aa0bdb1f586c5630d60b6237dfce45c";
 	xmlhttp.open("GET", url, true);
 	xmlhttp.send();
+
+	//once the request is accepted, process the fowllowing function to get data and complete the app information
 	xmlhttp.onreadystatechange = function() {
 	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-	      	var myArr = xmlhttp.responseText;
-	      	var text = myArr;
-	      	var json = JSON.parse(text);
-	      	document.getElementById('weather').innerHTML = "Today the weather is <b>" + json.weather[0].description + "</b> <img src = '//openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
-	      	
+	        var myArr = xmlhttp.responseText;
+	        var text = myArr;
+	        var json = JSON.parse(text);
+	        //alert(JSON.parse(text).coord.lon);
+	        //document.getElementById("id01").innerHTML = myArr;
+	    
+	        document.getElementById("weather").innerHTML = "Today the weather is <em><b>" + json.weather[0].main + "</b></em> </b> <img src = 'https://openweathermap.org/img/w/" + json.weather[0].icon + ".png'>";
+
 	    }
 	};
 }
